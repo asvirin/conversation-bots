bbc38da63ae95877a35a0033e69dff758915b556
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-06 13:45:22 +0300

    Update function_dialogflow.py

--- function_dialogflow.py
+++ function_dialogflow.py
@@ -3,4 +3,46 @@ from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.client import GoogleCredentials
 
+import dialogflow_v2 as dialogflow
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./key_for_dialogflow.json"
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print('Session path: {}\n'.format(session))
+
+    
+    text_input = dialogflow.types.TextInput(
+            text=text, language_code=language_code)
+
+    query_input = dialogflow.types.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+            session=session, query_input=query_input)
+        
+    return response.query_result.fulfillment_text
+  
+  
+  def create_intent(project_id, display_name, training_phrases_parts,
+                  message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = intents_client.project_agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.types.Intent.TrainingPhrase.Part(
+            text=training_phrases_part)
+        training_phrase = dialogflow.types.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.types.Intent.Message.Text(text=[message_texts])
+    message = dialogflow.types.Intent.Message(text=text)
+
+    intent = dialogflow.types.Intent(
+        display_name=display_name,
+        training_phrases=training_phrases,
+        messages=[message])
+
+    response = intents_client.create_intent(parent, intent)
