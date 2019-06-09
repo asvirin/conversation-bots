019c597ac94b88b0666b9d4ea843aeb657cf47b0
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-09 20:55:30 +0300

    Update bot-tg.py

--- bot-tg.py
+++ bot-tg.py
@@ -10,22 +10,9 @@ from oauth2client.client import GoogleCredentials
 
 import dialogflow_v2 as dialogflow
 
+from new_func import detect_intent_texts
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-
-def detect_intent_texts(project_id, session_id, text, language_code):
-    session_client = dialogflow.SessionsClient()
-
-    session = session_client.session_path(project_id, session_id)
-    
-    text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code)
-
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-            session=session, query_input=query_input)
-        
-    return response.query_result.fulfillment_text
 
 class MyLogsHandler(logging.Handler):
     def emit(self, record):
