4a9d905556433ae46dee9889d0a832a56797e52a
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-09 21:28:16 +0300

    Update curses_tools.py

--- curses_tools.py
+++ curses_tools.py
@@ -20,10 +20,18 @@ def detect_intent_texts(project_id, session_id, text, language_code):
 
     return response.query_result.fulfillment_text
 
-class MyLogsHandler(logging.Handler):
-    def emit(self, record):
-        telegram_token_information_message = os.environ['telegram_token_information_message']
-        chat_id_information_message = os.environ['chat_id_information_message']
-        log_entry = self.format(record)
-        bot_error = telegram.Bot(token=telegram_token_information_message)
-        bot_error.send_message(chat_id=chat_id_information_message, text=log_entry)
+#class MyLogsHandler(logging.Handler):
+#    def emit(self, record):
+#        telegram_token_information_message = os.environ['telegram_token_information_message']
+#        chat_id_information_message = os.environ['chat_id_information_message']
+#        log_entry = self.format(record)
+#        bot_error = telegram.Bot(token=telegram_token_information_message)
+#        bot_error.send_message(chat_id=chat_id_information_message, text=log_entry)
+        
+
+def MyLogsHandler(record):
+    telegram_token_information_message = os.environ['telegram_token_information_message']
+    chat_id_information_message = os.environ['chat_id_information_message']
+    log_entry = self.format(record)
+    bot_error = telegram.Bot(token=telegram_token_information_message)
+    bot_error.send_message(chat_id=chat_id_information_message, text=log_entry)
