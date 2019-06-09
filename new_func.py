65108c3bff3aeef1ff1db3d5967525b1478a679e
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-09 20:53:28 +0300

    Create new_func.py

---
+++ new_func.py
@@ -0,0 +1,21 @@
+import os
+import dialogflow_v2 as dialogflow
+
+if __name__ == '__main__':
+
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+
+    def detect_intent_texts(project_id, session_id, text, language_code):
+        session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(project_id, session_id)
+
+        text_input = dialogflow.types.TextInput(
+                text=text, language_code=language_code)
+
+        query_input = dialogflow.types.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+                session=session, query_input=query_input)
+
+        return response.query_result.fulfillment_text
