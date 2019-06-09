03b3e113e5d3e4d11ac1cbd6df195d7e67be57c3
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-09 21:20:43 +0300

    Update curses_tools.py

--- curses_tools.py
+++ curses_tools.py
@@ -1,5 +1,7 @@
 import os
 import dialogflow_v2 as dialogflow
+import telegram
+import logging 
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
@@ -17,3 +19,11 @@ def detect_intent_texts(project_id, session_id, text, language_code):
                 session=session, query_input=query_input)
 
     return response.query_result.fulfillment_text
+
+class MyLogsHandler(logging.Handler):
+    def emit(self, record):
+        telegram_token_information_message = os.environ['telegram_token_information_message']
+        chat_id_information_message = os.environ['chat_id_information_message']
+        log_entry = self.format(record)
+        bot_error = telegram.Bot(token=telegram_token_information_message)
+        bot_error.send_message(chat_id=chat_id_information_message, text=log_entry)
