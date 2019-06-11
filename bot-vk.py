9512597186b6650aaf4a283b96851059dd007378
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-11 11:57:44 +0300

    Update bot-vk.py

--- bot-vk.py
+++ bot-vk.py
@@ -13,13 +13,17 @@ def echo(event, vk_api):
     user_id = event.user_id
     user_message = event.text
     project_id = os.environ['project_id']
-    message = dialogflow_tools.detect_intent_texts(project_id, event.user_id, user_message, 'ru-RU')
-    if message:
-        vk_api.messages.send(
-            user_id=event.user_id,
-            message=message,
-            random_id=random.randint(1,1000000000)
-        )
+    
+    try:
+        message = dialogflow_tools.detect_intent_texts(project_id, event.user_id, user_message, 'ru-RU')
+        if message:
+            vk_api.messages.send(
+                user_id=event.user_id,
+                message=message,
+                random_id=random.randint(1,1000000000)
+            )
+    except Exception:
+        logger.exception("Проблема при получении и отправке сообщений")
 
 if __name__ == "__main__":
     logger = logging.getLogger()
