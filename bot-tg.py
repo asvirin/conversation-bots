3b3dd4f72f40e9caeb41d071b68796a65a33e893
asvirin <45921969+asvirin@users.noreply.github.com>
2019-06-11 11:31:01 +0300

    Update bot-tg.py

--- bot-tg.py
+++ bot-tg.py
@@ -10,14 +10,14 @@ from oauth2client.client import GoogleCredentials
 
 import dialogflow_v2 as dialogflow
 
-import curses_tools
-from curses_tools import MyLogsHandler
+import dialogflow_tools
+from dialogflow_tools import MyLogsHandler
         
 def echo(bot, update):
     chat_id = update.message.chat_id
     user_message = update.message.text
     project_id = os.environ['project_id']
-    bot_answer = curses_tools.detect_intent_texts(project_id, chat_id, user_message, 'ru-RU')
+    bot_answer = dialogflow_tools.detect_intent_texts(project_id, chat_id, user_message, 'ru-RU')
     update.message.reply_text(bot_answer)
     
 def start(bot, update):
